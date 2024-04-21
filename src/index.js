62f01ac27e907765b0b929aa5ff3b887873cfab9
Nelson Acosta <nelson@MacBook-Air-de-Nelson.local>
2024-04-22 00:50:51 +0200

    first_version

--- src/index.js
+++ src/index.js
@@ -1,23 +1,51 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
+function updateSubtotal(product) {     //, product_single_price, product_single_quantity
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  //sigo sin entender porque aqui podemos usar product y no document
+  const price = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector('.quantity input').value;
+
+  //let calculo = Number(price.textContent) * quantity; otra forma
+  let calculo = price * quantity;
+  
+  let subtotal = product.querySelector('.subtotal span');
+      subtotal.innerText = calculo;
+      return subtotal.innerText
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // ITERATION 2
-  //... your code goes here
+
+  //sigo sin entender poque no podemos usar product aquiiiiiii
+  const product_varios = document.getElementsByClassName("product");
+
+
+  let total = 0;
+  for( i=0 ; i<product_varios.length ; i++){
+  
+  const product_single_price = product_varios[i].querySelector('.price span');
+  const product_single_quantity = product_varios[i].querySelector('.quantity input');
+
+  const subtotal = updateSubtotal(product_varios[i], product_single_price, product_single_quantity);
+  
+  console.log(total);
+  console.log(subtotal);
+  total = parseFloat(total) + parseFloat(subtotal);
+  
+  }
 
   // ITERATION 3
-  //... your code goes here
+  
+    document.getElementById("total-value").textContent = "Total: $" + total;
+
+
+
 }
 
 // ITERATION 4
